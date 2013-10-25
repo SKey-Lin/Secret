49a57b33fd582669a6fc7369c559911240df3690
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-25 18:30:02 +0700

    Update Segitiga.cpp

--- Segitiga.cpp
+++ Segitiga.cpp
@@ -3,6 +3,13 @@
 
 void main()
 {
+	
+	printf("\t\t\t\t========================\n");
+	printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
+	printf("\t\t\t\t||         **         ||\n");
+	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
+	printf("\t\t\t\t========================\n\n\n");
+	
 	int n, i, a = 0, k=1, l=1, b=0, x;
 	printf("Masukan angka : ");
 	scanf("%d", &n); fflush(stdin);
