ab731144ebc0b25793fa08a944755b6fd996252d
SharKing-Lin <suv_asfly@yahoo.com>
2013-10-28 13:06:58 +0700

    Update Segitiga (Incomplete).cpp

--- Segitiga (Incomplete).cpp
+++ Segitiga (Incomplete).cpp
@@ -3,24 +3,29 @@
 
 void main()
 {
-	
 	printf("\t\t\t\t========================\n");
 	printf("\t\t\t\t|| %c%c%c 1701313990 %c%c%c ||\n", 6,3,5,5,3,6);
 	printf("\t\t\t\t||         **         ||\n");
 	printf("\t\t\t\t||  %c   SharKing   %c  ||\n", 6,6);
 	printf("\t\t\t\t========================\n\n\n");
-	
-	int n, i, a = 0, k=1, l=1, b=0, x;
-	printf("Masukan angka : ");
+
+	int n, i, a = 0, k=1, l=1;
+	printf("Masukan angka: ");
 	scanf("%d", &n); fflush(stdin);
 	for(i = 1; i <= (n*n); i++)
 	{
-		x=(n*k)+l;
-		if (i == x) //6, 11, 12, 16, 17, 18, 21, 22, 23, 24
+		if(i == (n*k)+l)
 		{
 			printf(" ");
-			k++;
-			l++;
+			if (l < k)
+			{
+				l++;
+			}
+			else if(l == k)
+			{
+				l=1;
+				k++;
+			}
 		}
 		else
 		{
@@ -33,17 +38,5 @@ void main()
 			a = 0;
 		}
 	}
-
-
 	getchar();
 }
-
-	/*for(k = 0; k < (i-1); k++)
-		{
-			
-		}
-
-		for(j = 0; j <= n-i; j++)
-		{
-			printf("*");
-		}*/
